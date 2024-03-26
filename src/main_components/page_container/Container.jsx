6cdb7b99b9f71bf1b5a16165b734b05d1e46a0f9
onnymm <phr2807@gmail.com>
2024-03-25 17:58:56 -0700

    Arreglado el contenedor principal

--- src/main_components/page_container/Container.jsx
+++ src/main_components/page_container/Container.jsx
@@ -3,12 +3,14 @@ import ProductItem from "../product_item/ProductItem";
 import style from "./Container.module.css";
 
 const Container = ({ sidebarShow }) => (
-	<main
-		className={`${style.container}${sidebarShow ? ` ${style.sidebarActive}` : ""}`}
-	>
-		{DATA.map((item, index) => (
-			<ProductItem key={index} {...item} />
-		))}
+	<main className={style.main}>
+		<div
+			className={`${style.container}${sidebarShow ? ` ${style.sidebarActive}` : ""}`}
+		>
+			{DATA.map((item, index) => (
+				<ProductItem key={index} {...item} />
+			))}
+		</div>
 	</main>
 );
 
